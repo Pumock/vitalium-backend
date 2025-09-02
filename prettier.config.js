2a94a1d8d8e1700bab73da829eeca0249b441728
Luiz Felipe <luizfelipbslima@gmail.com>
2025-09-02 00:27:56 -0300

    chore: configurar Prettier e ESLint no projeto\n\nEste commit adiciona e configura as ferramentas Prettier e ESLint, garantindo padronização de código e correção automática de formatação e erros comuns. Isso melhora a qualidade do código e facilita a colaboração entre desenvolvedores.\n\n- Adicionado ESLint com regras recomendadas\n- Integrado Prettier ao ESLint\n- Criado arquivo prettier.config.js com definições de estilo\n- Configurado .vscode/settings.json para correção automática ao salvar\n- Atualizado package.json com scripts de lint e fix\n\nResolve: 1

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'lf',
+};
